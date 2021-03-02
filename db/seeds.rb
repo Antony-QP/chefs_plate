302cca3dcfcf83874fedcd2825c5c626e41fe7be
Antony Parker <antonyqparker@gmail.com>
2021-03-02 12:12:47 +0100

    creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 user_1 = User.new({
     email: "antony@gmail.com",
     password: "123456"
@@ -42,4 +44,114 @@ post_1 = Post.new({
 follower_1 = Follower.new({
     user_id: 1,
     chef_id: 1
+})
+
+chef_10 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 10
+})
+
+chef_11 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 11
+})
+
+chef_12 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 12
+})
+
+chef_13 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 13
+})
+
+chef_14 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 14
+})
+
+chef_15 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 15
+})
+
+chef_16 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 16
+})
+
+chef_17 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 17
+})
+
+chef_18 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 18
+})
+
+chef_19 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 19
+})
+
+chef_20 = Chef.new({
+    first_name: "Johnny",
+    last_name: "Smith",
+    email: "johnny@chef.com",
+    inspiration: "blah blah blah",
+    work_history: "blah blah blah",
+    mission: "blah blah blah",
+    restaurant_id: 20
 })
