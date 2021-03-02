249809721a1d3daf7a27f70da1b2813e2f1e2e07
Antony Parker <antonyqparker@gmail.com>
2021-03-02 15:55:14 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
-# user_1 = User.new({
-#     email: "antony@gmail.com",
-#     password: "123456"
-# })
-
-# chef_1 = Chef.new({
-#     first_name: "Johnny",
-#     last_name: "Smith",
-#     email: "johnny@chef.com",
-#     inspiration: "blah blah blah",
-#     work_history: "blah blah blah",
-#     mission: "blah blah blah",
-#     restaurant_id: 1
-# })
-
-# menu_1 = Menu.new({
-#     title: "Spring menu",
-#     price: 50,
-#     chefs_note: "This is a very delicious menu",
-#     cuisine: "French",
-#     chef_id: 1
-# })
-
-# post_1 = Post.new({
-#     description: "This is a description of a post",
-#     chef_id: 1
-# })
-
-# follower_1 = Follower.new({
-#     user_id: 1,
-#     chef_id: 1
-# })
 puts "Cleaning database"
 User.destroy_all
 Follower.destroy_all
