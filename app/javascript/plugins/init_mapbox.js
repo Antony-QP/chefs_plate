1e20a513bc93e361b5cfffcfac564df4aaf5c826
Daan Dalman <daandalman@gmail.com>
2021-03-03 01:39:16 +0100

    create working geomap

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,8 +13,19 @@ const initMapbox = () => {
         });
         const markers = JSON.parse(mapElement.dataset.markers);
         markers.forEach((marker) => {
-            new mapboxgl.Marker()
+            const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+            const element = document.createElement("div");
+            element.className = "marker";
+            element.style.backgroundImage = `url('${marker.image_url}')`;
+            element.style.backgroundSize = "contain";
+            element.style.width = "25px";
+            element.style.height = "22px";
+            element.style.fill = "#FF395B"
+
+            new mapboxgl.Marker(element)
                 .setLngLat([marker.lng, marker.lat])
+                .setPopup(popup) // add this
                 .addTo(map);
         });
         const fitMapToMarkers = (map, markers) => {
