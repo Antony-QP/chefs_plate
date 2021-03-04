f5539c45ca24f5d47d97e41a6817c2644ca4624d
Antony Parker <antonyqparker@gmail.com>
2021-03-04 16:20:40 +0100

    Creating dashboard and menu/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :chefs, only: [:index, :show] do
-    resources :followers, only: [:create]
     resources :menus, only: [:index, :show]
     resources :posts, only: [:index]
+    resources :followers, only: [:new, :create]
   end
+  resources :followers, only: [:index, :destroy]
 end
