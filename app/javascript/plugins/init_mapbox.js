5f5dde34c549838db8b903cfc13ebe06d7b01601
Daan Dalman <daandalman@gmail.com>
2021-03-08 10:12:44 +0100

    adjusting padding for map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -35,7 +35,7 @@ const initMapbox = () => {
         const fitMapToMarkers = (map, markers) => {
             const bounds = new mapboxgl.LngLatBounds();
             markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-            map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+            map.fitBounds(bounds, { padding: 50, maxZoom: 25, duration: 0 });
         };
         map.addControl(new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
