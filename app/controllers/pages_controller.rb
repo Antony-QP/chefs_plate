7656c66acf3cd39f47443177e6d6aad65b65056e
Frans Scholten <fjdscholten@gmail.com>
2021-03-09 14:34:36 +0100

    chef it 2.0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
     @chefs = Chef.limit(4)
   end
 
+
   def chefit
-    @chefs = Chef.all.sample(3)
+    @top_chefs = ChefRating.select { |chef| chef.average_rating > 4}
+    # using a select method iterator to only filter the chefs with average rating more than 4.
   end
 end
