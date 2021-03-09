94e220a02a9aeee0a9089ad82897c36d4dcfba12
Antony Parker <antonyqparker@gmail.com>
2021-03-09 16:42:45 +0100

    Searching on hompage

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -5,13 +5,10 @@ const initStarRating = () => {
     $('#chef_rating_rating').barrating({
       theme: 'css-stars',
       onSelect: (value, text, event) => {
-        const form = $("form.review_form"); // We are selecting the form on the page with its class
-        form.submit(); // We submit the form with javascript
+        const form = $("form.review_form"); 
+        form.submit(); 
       }
   });
-    // console.log(chefSelect)
-    // console.log(barrating)
-    // chefSelect
   };
 
 export { initStarRating };
