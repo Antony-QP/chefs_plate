31ed3b05ef6abb35299e9701b8d7c72d9aece42a
Daan Dalman <daandalman@gmail.com>
2021-03-10 12:14:27 +0100

    last version of booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   # belongs_to :chef, through: :restaurant
   TIMES = %w(18.00 19.00 20.00 21.00 22.00)
-  validates :times, inclusion: { in: TIMES }
+  validates :start_time, inclusion: { in: TIMES }
 end
