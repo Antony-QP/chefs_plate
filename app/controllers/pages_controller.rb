592b7d9f008c765abd1b4f43279df094ade7a8f8
Frans Scholten <fjdscholten@gmail.com>
2021-03-10 14:09:51 +0100

    typo homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,11 @@ class PagesController < ApplicationController
   end
 
   def chefit
-
-    @restaurants = Restaurant.near([current_user.latitude, current_user.longitude], 50)
+    @restaurants = Restaurant.near([current_user.latitude, current_user.longitude], 200)
 
     @top_chefs = Chef.where(restaurant: @restaurants.map(&:id)).select do |chef|
      chef.average_rating > 4
     end
-
     # using a select method iterator to only filter the chefs with average rating more than 4.
   end
 end
