c087be1e0554dd5334af0b711cd81c7cade5254f
Daan Dalman <daandalman@gmail.com>
2021-03-31 14:38:52 +0200

    create chhefs login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def authenticate_admin!
+        authenticate_user!
+        redirect_to :somewhere, status: :forbidden unless current_user.admin?
+      end
+
     protected
 
     def configure_permitted_parameters
